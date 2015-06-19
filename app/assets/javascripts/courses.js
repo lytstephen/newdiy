e21087d9a70ba6c6dee30c639c717ad09a62b346
Stephen Leung <lytstephen@gmail.com>
2015-06-19 11:16:35 +0800

    add modal photo function to course images

--- app/assets/javascripts/courses.js
+++ app/assets/javascripts/courses.js
@@ -2,9 +2,23 @@ var ready;
 
 ready = function() {
 
+	// toggle active categories on AJAX filter
 	$('#categories li').click( function() {
 		$('#categories li.active').removeClass();
 		$(this).addClass('active');
+	});
+
+	// photo click to enlarge
+	$('.modalphotos .course_image').on('click', function() {
+		$('#modal').modal({
+			show: true,
+		})
+
+		var mysrc = this.src.replace('thumbnail', 'large')
+		$('#modalimage').attr('src', mysrc);
+		$('.modal-body').on('click', function() {
+			$('#modal').modal('hide');
+		})
 	});
 
 }
